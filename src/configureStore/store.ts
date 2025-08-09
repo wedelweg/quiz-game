8399adbd76d3b69ce3d651e69858825c34ec4f71
seilah <princenoru@gmail.com>
2025-08-09 20:17:08 +0200

    redux-add-scoring: added redux-devtools support

--- src/configureStore/store.ts
+++ src/configureStore/store.ts
@@ -1,16 +1,18 @@
-import {legacy_createStore as createStore, type Store} from "redux";
-import {userReducer} from "../reducers/userReducer.js";
+import {legacy_createStore as createStore} from "redux";
+import {userReducer} from "../reducers/userReducer.ts";
 import type {StateInterface} from "../utils/types.ts";
+import {composeWithDevTools} from "@redux-devtools/extension";
 
+const enhancer = import.meta.env.DEV ? composeWithDevTools() : undefined;
 
 export const initialState: StateInterface = {
     user: {
-       login: '',
-        password: ''
+        login: "name",
+        password: "",
     },
     scores: {
         score: 0,
     }
-};
+}
 
-export const store: Store<StateInterface> = createStore(userReducer, initialState);
+export const store = createStore(userReducer, initialState, enhancer);
