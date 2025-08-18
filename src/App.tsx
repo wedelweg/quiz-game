653450a88d1dcd108d007517cbb9595eac58cec5
seilah <princenoru@gmail.com>
2025-08-18 17:48:11 +0200

    redux-add-scoring-merge-with-firebase: merge and conflict resolve

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import './App.css';
 import {Route, Routes} from "react-router";
 import Game from "./components/Game.tsx";
 import Login from "./components/Login.tsx";
-import {ScoreProvider} from "./components/ScoreContext.tsx";
 import {Provider} from "react-redux";
 import {store} from "./configureStore/store.ts";
 import {collection, getDocs} from "firebase/firestore";
@@ -27,12 +25,10 @@ const App = () => {
         <div className="min-h-screen bg-gradient-to-b from-[#1a1a4f] to-[#000032]
             text-white p-4 flex flex-col items-center gap-6">
             <Provider store={store}>
-                <ScoreProvider>
                     <Routes>
                         <Route path="/game" element={<Game/>}/>
                         <Route path="/" element={<Login/>}/>
                     </Routes>
-                </ScoreProvider>
             </Provider>
         </div>
     );
