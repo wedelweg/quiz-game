66527cd17298185ed1bab9c5135a8f5b87f25dde
01452 <102763331+01452@users.noreply.github.com>
2025-08-21 19:01:45 +0200

    init

--- src/features/scoreData/scoreSlice.ts
+++ src/features/scoreData/scoreSlice.ts
@@ -1,24 +1,50 @@
-import {createSlice, type PayloadAction} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import type {ScoreStateInterface} from "../../utils/types.ts";
+import {doc, updateDoc} from "firebase/firestore";
+import {db} from "../../data/firestore.ts";
 
 const initialState: ScoreStateInterface = {
     scores: {
         score: 0
     }
 }
+interface ScoreWithId {
+    price:number,
+    oldScore:number,
+    id:string
+}
+
+export const fetchScoreUpdateDB = createAsyncThunk(
+    "user/fetchScoreUpdateDB",
+    async ({price,oldScore,id}:ScoreWithId)=>{
+        const userRef = doc(db, 'users', id);
+        if (!userRef){
+            throw new Error("Not found 404");
+        }
+        const newScore = oldScore + price;
+        await updateDoc(userRef, {score: newScore});
+        return newScore;
+    }
+);
+
 
 const scoreSlice = createSlice({
     name: 'score',
     initialState,
     reducers: {
-        increaseScore: (state, action:PayloadAction<number>) => {
-            state.scores.score += action.payload
-        },
-        decreaseScore: (state, action) => {
-            state.scores.score -= action.payload
-        }
+    },
+    extraReducers: (builder)=>{
+        builder
+            .addCase(fetchScoreUpdateDB.pending, (state)=>{
+                state.scores = {score: NaN};
+        })
+            .addCase(fetchScoreUpdateDB.fulfilled, (state, {payload})=>{
+                state.scores = {score: payload};
+            })
+            .addCase(fetchScoreUpdateDB.rejected, ()=>{
+                console.log("Error!")
+            })
     }
 })
 
-export const {increaseScore, decreaseScore} = scoreSlice.actions;
 export default scoreSlice.reducer;
